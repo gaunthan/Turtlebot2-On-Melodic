d83600a56245743cc9af260069904b0278230f1a
gaunthan <gaunthanhuang@foxmail.com>
2019-03-22 21:54:03 +0800

    Add script for building most basic environment for controling Turtlebot2 by keyboard

---
+++ build_basic.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+mkdir -p src
+cd src
+
+git clone https://github.com/turtlebot/turtlebot.git
+git clone https://github.com/turtlebot/turtlebot_msgs.git
+
+git clone https://github.com/yujinrobot/kobuki_msgs.git
+git clone https://github.com/yujinrobot/kobuki.git
+mv kobuki/kobuki_description kobuki/kobuki_node \
+  kobuki/kobuki_keyop kobuki/kobuki_safety_controller \
+  kobuki/kobuki_bumper2pc ./
+rm -rf kobuki
+
+git clone https://github.com/yujinrobot/yujin_ocs.git
+mv yujin_ocs/yocs_cmd_vel_mux yujin_ocs/yocs_controllers .
+rm -rf yujin_ocs
+
+sudo apt-get install ros-melodic-kobuki-* -y
+sudo apt-get install ros-melodic-ecl-streams -y
+
+cd ..
+catkin_make
