99ce980de9155548a879c5151630ac5bfa487c32
gaunthan <gaunthanhuang@foxmail.com>
2019-03-22 21:55:03 +0800

    Add script for building development environment for Turtlebot2

---
+++ build_all.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+mkdir -p src
+cd src
+
+git clone https://github.com/turtlebot/turtlebot_simulator
+git clone https://github.com/turtlebot/turtlebot.git
+git clone https://github.com/turtlebot/turtlebot_apps.git
+git clone https://github.com/turtlebot/turtlebot_msgs.git
+git clone https://github.com/turtlebot/turtlebot_interactions.git
+
+git clone https://github.com/toeklk/orocos-bayesian-filtering.git
+cd orocos-bayesian-filtering/orocos_bfl/
+./configure
+make
+sudo make install
+cd ../
+make
+cd ../
+
+git clone https://github.com/udacity/robot_pose_ekf
+git clone https://github.com/ros-perception/depthimage_to_laserscan.git
+
+git clone https://github.com/yujinrobot/kobuki_msgs.git
+git clone https://github.com/yujinrobot/kobuki_desktop.git
+cd kobuki_desktop/
+rm -r kobuki_qtestsuite
+cd -
+git clone https://github.com/yujinrobot/kobuki.git
+mv kobuki/kobuki_description kobuki/kobuki_bumper2pc \
+  kobuki/kobuki_node kobuki/kobuki_keyop \
+  kobuki/kobuki_safety_controller ./
+
+git clone https://github.com/yujinrobot/yujin_ocs.git
+mv yujin_ocs/yocs_cmd_vel_mux yujin_ocs/yocs_controllers .
+rm -rf yujin_ocs
+
+sudo apt-get install ros-melodic-kobuki-* -y
+sudo apt-get install ros-melodic-ecl-streams -y
+
+cd ..
+catkin_make
