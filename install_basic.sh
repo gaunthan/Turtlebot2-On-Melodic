44e3ffdc103d563eef9457da5903a1a478862277
Hanzhe Teng <hteng007@ucr.edu>
2020-10-11 12:53:24 -0700

    add missing dependencies

--- install_basic.sh
+++ install_basic.sh
@@ -9,7 +9,7 @@ cd src
 git clone https://github.com/turtlebot/turtlebot.git
 git clone https://github.com/turtlebot/turtlebot_msgs.git
 git clone https://github.com/turtlebot/turtlebot_apps.git
-git clone https://github.com/turtlebot/turtlebot_simulator
+git clone https://github.com/turtlebot/turtlebot_simulator.git
 
 git clone https://github.com/yujinrobot/kobuki_msgs.git
 git clone --single-branch --branch melodic https://github.com/yujinrobot/kobuki.git
@@ -17,6 +17,10 @@ mv kobuki/kobuki_description kobuki/kobuki_node \
   kobuki/kobuki_keyop kobuki/kobuki_safety_controller \
   kobuki/kobuki_bumper2pc ./
 rm -rf kobuki
+
+git clone --single-branch --branch melodic https://github.com/yujinrobot/kobuki_desktop.git
+mv kobuki_desktop/kobuki_gazebo_plugins ./
+rm -rf kobuki_desktop
 
 git clone https://github.com/yujinrobot/yujin_ocs.git
 mv yujin_ocs/yocs_cmd_vel_mux yujin_ocs/yocs_controllers .
@@ -28,3 +32,4 @@ sudo apt-get install ros-melodic-ecl-streams -y
 # necessary for build and gazebo
 sudo apt-get install ros-melodic-depthimage-to-laserscan -y
 sudo apt-get install ros-melodic-joy -y
+sudo apt-get install ros-melodic-yocs-velocity-smoother -y
